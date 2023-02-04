79037221be3f6049879882672e999c59a3b7cfa4
Grace Thompson <grace.thompson97@gmail.com>
2023-02-04 12:29:03 -0800

    trying to add api key without public exposure

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-//needed packages
+//needed packages - already accomplished with package.json (?)
 //declare constants 
+const API_KEY = process.env.API_KEY
 //function to pull contents of text file into memory as array of words
 //function to randomly select items from these arrays
 //function to randomly select font
