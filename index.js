b73066003a673d148e75402d445e97a9ba11d186
Grace Thompson <grace.thompson97@gmail.com>
2023-02-04 12:33:17 -0800

    setting up a simple request

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 //needed packages - already accomplished with package.json (?)
+import { createClient } from 'pexels';
 //declare constants 
-const API_KEY = process.env.API_KEY
+const API_KEY = process.env.API_KEY;
+const CLIENT = createClient(API_KEY);
 //function to pull contents of text file into memory as array of words
 //function to randomly select items from these arrays
 //function to randomly select font
