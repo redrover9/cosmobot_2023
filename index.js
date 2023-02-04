e973334cd8e4398cbfb4c96cb49955b9b4aada07
Grace Thompson <grace.thompson97@gmail.com>
2023-02-04 12:13:29 -0800

    adding comments

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 //needed packages
 //declare constants 
-//function to 
+//function to pull contents of text file into memory as array of words
+//function to randomly select items from these arrays
+//function to create a caption
+//function to get photo url
