6f0d91238f0cb110a5417a485ddc9a49091597f7
Grace Thompson <grace.thompson97@gmail.com>
2023-02-05 16:41:09 -0800

    remove the brokenness

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-//needed packages - already accomplished with package.json (?)
-import './node_modules/pexels/dist/main.js';
-//declare constants 
-const API_KEY = process.env.API_KEY;
-const CLIENT = createClient(API_KEY);
-//test
-CLIENT.photos.show({ id: 2014422 }).then(photo => {console.log(photo)});
+let api_key = process.env.API_KEY;
+
+
+
+
+
+
+
+
+
+
+
+
+
 //function to pull contents of text file into memory as array of words
 //function to randomly select items from these arrays
 //function to randomly select font
